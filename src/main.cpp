7d09a46a159d03183499053e360c78de7035c8f9
MihaKon <konon.michal@gmail.com>
2023-11-25 15:45:27 +0100

    update src

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,173 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
 
-struct Joint {
+struct Graph
+{
     std::string name;
-    int vFrom;
-    int vTo;
-}
+    int numberOfVertexes = 3;
+    int numberOfJoints;
+    std::vector<std::vector<int>> adjList;
+    bool isCoupled = false;
+    bool isLinear = false;
+};
 
-struct Graph{
-	std::string name;
-	int numberOfVertexes;
-	int numberOfJoints;
-	std::vector<Joint> joints;
-}
-
-class GraphReader{
+class GraphReader
+{
 private:
     std::string fileName;
     std::vector<Graph> graphs;
     std::vector<Graph> convertedGraphs;
+
 public:
-    GraphReader(std::string fileName){
-        fileName = fileName;
+    GraphReader(std::string file)
+    {
+        fileName = file;
     }
-    GraphReader(char fileName){
-        fileName = fileName;
-    }
-    void readFile(){
-        if(fileName.empty()){
-            std::cout << "No file name parsed!" << std::endl;
-            return;
-        }
-        std::fstream fileGraph;
+    void readFile()
+    {
+        std::ifstream fileGraph;
         fileGraph.open(fileName, std::ios::in);
-        if (!fileGraph) {
-            std::cout << "No such file" << std::endl;;
+        if (!fileGraph.is_open())
+        {
+            std::cout << "No such file" << std::endl;
             return;
         }
-        std::string fileContent;
-        while (1) {
-			fileGraph >> fileContent;
-			if (fileGraph.eof())
-				break;
+        std::string line;
+        while (std::getline(fileGraph, line))
+        {
+            Graph graph;
+            if (line.empty())
+                continue;
+            if (line[0] == '>')
+            {
+                std::istringstream iss(line.substr(1));
+                std::string nv, nj;
+                std::getline(iss, graph.name, ',');
+                std::getline(iss, nv, ',');
+                std::getline(iss, nj, ',');
+                graph.numberOfJoints = std::stoi(nv);
+                graph.numberOfVertexes = std::stoi(nj);
+            }
+            else
+            {
+                
+            }
+            graphs.push_back(graph);
         }
-        fileContent.getline();
         fileGraph.close();
     }
-    void runForEeveryGraph() {
-        for (Graph graph : graphs) {
-            isCoupled(graph);
-            isLIne(graph);
-            convertToCoupled(Graph graph);
-        }
+    bool checkCommon(std::vector<int> vectorOne, std::vector<int> vectorTwo)
+    {
+        return std::find_first_of(vectorOne.begin(), vectorOne.end(), vectorTwo.begin(), vectorTwo.end()) != vectorOne.end();
     }
-    bool isCoupled(Graph graph){}
-    bool isLIne(Graph graph) {
-        return result;
-    }
-    void convertToCoupled(Graph graph) {}
-    void writeFile(){
-        std::string toFileName;
-        std:: fstream fileGraph;
-        fileGraph.open(toFileName, std::ios::out);
-        if (!fileGraph) {
-            std::cout << "Cannot create/open " << toFileName << std::endl;
-            return
-        }
-        for (Graph graph : graphs) {
-            fileGraph << ">" << graph.name << ",";
-            fileGraph << graph.numberOfVertexes << "," << graph.numberOfJoints << "\n";
-            for (Joint joint : graph.joints) {
-                fileGraph << "(";
-                if(joint.name) {
-                    fileGraph << joint.name << ",";
-                }
-                fileGraph << joint.vFrom;
-                if(joint.vTo) {
-                    fileGraph << "-" << joint.vTo;
-                }
-                fileGraph << "),";
+    bool isGraphCoupled(Graph graph)
+    {
+        for (int i = 0; i < graph.numberOfVertexes; i++)
+        {
+            if (graph.adjList[i].empty())
+                continue;
+            const bool hasDuplicates = std::adjacent_find(graph.adjList[i].begin(), graph.adjList[i].end()) != graph.adjList[i].end();
+            if (hasDuplicates)
+                return false;
+            for (int j = i + 1; j < graph.numberOfVertexes; j++)
+            {   
+                bool common = checkCommon(graph.adjList[i],graph.adjList[j]);
+                if (common && !graph.adjList[j].empty()&& graph.adjList[i] != graph.adjList[j])
+                    return false;
             }
+        }
+        return true;
+    }
+    bool isGraphLinear(Graph graph)
+    {
+        for (int i = 0; i < graph.numberOfVertexes; i++)
+        {
+            const bool hasDuplicates = std::adjacent_find(graph.adjList[i].begin(), graph.adjList[i].end()) != graph.adjList[i].end();
+            if (hasDuplicates)
+                return false;
+            for (int j = i + 1; j < graph.numberOfVertexes; j++)
+            {
+                if (!graph.adjList[j].empty() && graph.adjList[i] != graph.adjList[j])
+                    return false;
+            }
+        }
+        return true;
+    }
+    void convert(Graph graph)
+    {
+        Graph convertedGraph;
+        for (int i = 0; i < graph.numberOfVertexes * 2; i++)
+        {
+            if (i % 2 == 0 || i == 0)
+                convertedGraph.adjList.push_back(std::vector<int>{i + 1});
+            else
+                convertedGraph.adjList.push_back(std::vector<int>{});
+        }
+        bool isOdd = true;
+        for (int i = 0; i < graph.numberOfVertexes; i++)
+        {
+            if (graph.adjList[i].empty())
+                continue;
+            for (int j = 0; j < graph.adjList[i].size(); j++)
+            {
+                int vertex = graph.adjList[i][j];
+                convertedGraph.adjList[vertex].push_back(vertex + 1);
+                convertedGraph.adjList[vertex + 1] = {};
+                convertedGraph.adjList.erase(convertedGraph.adjList.begin() + 2);
+            }
+        }
+    }
+    void writeFile()
+    {
+        std::string toFileName = "result_" + fileName;
+        std::ofstream fileGraph;
+        fileGraph.open(toFileName, std::ios::out);
+        if (!fileGraph)
+        {
+            std::cout << "Cannot create/open " << toFileName << std::endl;
+            return;
+        }
+
+        for (Graph graph : graphs)
+        {
+            if (graph.name.empty())
+                continue;
+
+            fileGraph << graph.name << ",";
+            fileGraph << graph.numberOfVertexes << "," << graph.numberOfJoints << "\n";
+            // TODO: ADD reading adj matrix
             fileGraph << ";\n";
         }
         fileGraph.close();
     }
+    void runForEeveryGraph()
+    {
+        for (Graph graph : graphs)
+        {
+            graph.isCoupled = isGraphCoupled(graph);
+            if (graph.isCoupled)
+            {
+                graph.isLinear = isGraphLinear(graph);
+                convert(graph);
+            }
+        }
+    }
 };
 
-int main(int argc, char* argv[]) {
-    std::string name;
-    if(argc > 0 && argc < 2) {
-        name = argv[0];
-    } else( argv > 1) {
-        name = "Test.txt";
-        std::cout << "Cannot read argument, using default preset.File name: "<< name << std::endl;
-    }
+int main(int argc, char *argv[])
+{
+    std::string name = "Test.txt";
     GraphReader test(name);
     std::cout << "Reading " << name << std::endl;
     test.readFile();
-    std::cout << "Checking" << std::endl;
-    test.runForEeveryGraph();
-    std::cout << "Saving results" << std::endl;
-    test.writeFile();
-    std::cout << "END" << std::endl; 
+    // std::cout << "Checking" << std::endl;
+    // test.runForEeveryGraph();
+    // // std::cout << "Saving results" << std::endl;
+    // // test.writeFile();
+    // std::cout << "END" << std::endl;
     return 0;
 }
