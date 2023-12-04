11a456c033380ec0ec073f4442fc9955e3de4859
MihaKon <konon.michal@gmail.com>
2023-12-04 22:13:57 +0100

    update: main.cpp remove file handling from class

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,64 @@
 #include <sstream>
 #include <algorithm>
 
+void writeFile(std::string fileName, const Graph& graph)
+{
+    std::string toFileName = "result_" + fileName;
+    std::ofstream fileGraph;
+    fileGraph.open(toFileName, std::ios::out);
+    if (!fileGraph)
+    {
+        std::cout << "Cannot create/open " << toFileName << std::endl;
+        return;
+    }
+
+    for (Graph graph : graphs)
+    {
+        if (graph.name.empty())
+            continue;
+
+        fileGraph << graph.name << ",";
+        fileGraph << graph.numberOfVertexes << "," << graph.numberOfJoints << "\n";
+        // TODO: ADD reading adj matrix
+        fileGraph << ";\n";
+    }
+    fileGraph.close();
+}
+
+void readFile(std::string filename)
+{
+    std::ifstream fileGraph;
+    fileGraph.open(fileName, std::ios::in);
+    if (!fileGraph.is_open())
+    {
+        std::cout << "No such file" << std::endl;
+        return;
+    }
+    std::string line;
+    while (std::getline(fileGraph, line))
+    {
+        Graph graph;
+        if (line.empty())
+            continue;
+        if (line[0] == '>')
+        {
+            std::istringstream iss(line.substr(1));
+            std::string nv, nj;
+            std::getline(iss, graph.name, ',');
+            std::getline(iss, nv, ',');
+            std::getline(iss, nj, ',');
+            graph.numberOfJoints = std::stoi(nv);
+            graph.numberOfVertexes = std::stoi(nj);
+        }
+        else
+        {
+            
+        }
+        graphs.push_back(graph);
+    }
+    fileGraph.close();
+}
+
 struct Graph
 {
     std::string name;
@@ -18,47 +76,12 @@ struct Graph
 class GraphReader
 {
 private:
-    std::string fileName;
     std::vector<Graph> graphs;
     std::vector<Graph> convertedGraphs;
 
 public:
-    GraphReader(std::string file)
-    {
-        fileName = file;
-    }
-    void readFile()
-    {
-        std::ifstream fileGraph;
-        fileGraph.open(fileName, std::ios::in);
-        if (!fileGraph.is_open())
-        {
-            std::cout << "No such file" << std::endl;
-            return;
-        }
-        std::string line;
-        while (std::getline(fileGraph, line))
-        {
-            Graph graph;
-            if (line.empty())
-                continue;
-            if (line[0] == '>')
-            {
-                std::istringstream iss(line.substr(1));
-                std::string nv, nj;
-                std::getline(iss, graph.name, ',');
-                std::getline(iss, nv, ',');
-                std::getline(iss, nj, ',');
-                graph.numberOfJoints = std::stoi(nv);
-                graph.numberOfVertexes = std::stoi(nj);
-            }
-            else
-            {
-                
-            }
-            graphs.push_back(graph);
-        }
-        fileGraph.close();
+    void addGraph(Graph graph){
+        graphs.push_back(graph);
     }
 
     bool checkCommon(const std::vector<int>& vectorOne, const std::vector<int>& vectorTwo)
@@ -119,6 +142,7 @@ public:
         }
         return true;
     }
+    
     void convert(const Graph& graph)
     {
         Graph convertedGraph;
@@ -130,29 +154,7 @@ public:
                 convertedGraph.adjList.push_back(std::vector<int> {});
         }
     }
-    void writeFile()
-    {
-        std::string toFileName = "result_" + fileName;
-        std::ofstream fileGraph;
-        fileGraph.open(toFileName, std::ios::out);
-        if (!fileGraph)
-        {
-            std::cout << "Cannot create/open " << toFileName << std::endl;
-            return;
-        }
 
-        for (Graph graph : graphs)
-        {
-            if (graph.name.empty())
-                continue;
-
-            fileGraph << graph.name << ",";
-            fileGraph << graph.numberOfVertexes << "," << graph.numberOfJoints << "\n";
-            // TODO: ADD reading adj matrix
-            fileGraph << ";\n";
-        }
-        fileGraph.close();
-    }
     void runForEeveryGraph()
     {
         for (Graph graph : graphs)
@@ -170,13 +172,12 @@ public:
 int main(int argc, char *argv[])
 {
     std::string name = "Test.txt";
-    GraphReader test(name);
+    GraphReader test;
     std::cout << "Reading " << name << std::endl;
-    test.readFile();
+    readFile(name);
     std::cout << "Checking" << std::endl;
     test.runForEeveryGraph();
     // std::cout << "Saving results" << std::endl;
-    // test.writeFile();
     std::cout << "END" << std::endl;
     return 0;
 }
